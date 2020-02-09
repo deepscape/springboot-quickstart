ae71ba45bbfffac6f5717c52ba5f0e2f150d7192
deepscape <hdkim.scala@gmail.com>
2020-02-09 21:42:39 +0900

    member 테스트 케이스 (데이터 초기화)

---
+++ chapter06/src/main/java/com/rubypaper/controller/LoginController.java
@@ -0,0 +1,18 @@
+package com.rubypaper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+
+@Controller
+public class LoginController {
+
+	@GetMapping("/login")
+	public void loginView() {
+	}
+
+}
